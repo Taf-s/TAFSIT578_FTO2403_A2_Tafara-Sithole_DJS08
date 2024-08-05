c9755f04f94a32d914587b1d805f333b0c3f070e
taf-s <tafara.sithole@gmail.com>
2024-08-06 01:47:01 +0200

    added content and image to about component

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   </header>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="./about" element={<About />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   </BrowserRouter>
   )
