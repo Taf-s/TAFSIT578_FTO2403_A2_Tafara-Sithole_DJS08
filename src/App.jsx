6e867c85ea310f89d8f3b2a56b8730a100fac97f
taf-s <tafara.sithole@gmail.com>
2024-08-06 19:14:49 +0200

    added routing and rendering for header and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import Vans from './Pages/Vans'
 import VansDetail from './Pages/VansDetail'
+import Layout from './Components/Layout'
 
 import "./server"
 
@@ -11,19 +12,15 @@ function App() {
 
   return (
     <BrowserRouter>
-      <header>
-    <Link className="site-logo" to="/">#VanLife</Link>
-    <nav>
-      <Link to="/about">About</Link>
-      <Link to="/vans">Vans</Link>
-    </nav>
-  </header>
     <Routes>
+    <Route element={<Layout />}>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/vans" element={<Vans />} />
       <Route path="/vans/:id" element={<VansDetail />} />
+    </Route> 
     </Routes>
+   
   </BrowserRouter>
   )
 }
