39c99505e4e35a1eec83f4264ec191340787ddf5
taf-s <tafara.sithole@gmail.com>
2024-08-06 17:03:11 +0200

    pulled data and set the state of the vans page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
+import Vans from './Pages/Vans'
+
+import "./server"
 
 function App() {
 
@@ -11,11 +14,13 @@ function App() {
     <Link className="site-logo" to="/">#VanLife</Link>
     <nav>
       <Link to="/about">About</Link>
+      <Link to="/vans">Vans</Link>
     </nav>
   </header>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/vans" element={<Vans />} />
     </Routes>
   </BrowserRouter>
   )
