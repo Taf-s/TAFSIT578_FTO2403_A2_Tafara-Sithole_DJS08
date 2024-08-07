9011f1a9e0f6b494d2952789b54c3ab738971bb8
taf-s <tafara.sithole@gmail.com>
2024-08-07 22:04:48 +0200

    added back to all vans link

--- src/Pages/Host/HostVanDetail.jsx
+++ src/Pages/Host/HostVanDetail.jsx
@@ -1,15 +1,46 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 
-function HostVanDetail() {
+ function HostVanDetail() {
+
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans))
+    }, [])
+
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
+
+        
     return (
-        <div>
-            <h1>Host Van Detail</h1>
+        <section>
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
 
-        </div>
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
-
-
 }
 
 export default HostVanDetail
