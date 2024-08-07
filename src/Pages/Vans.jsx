5547848eeca33a018c58cf9c7907bf9f8fcd1be4
taf-s <tafara.sithole@gmail.com>
2024-08-08 01:27:28 +0200

    added back to link

--- src/Pages/Vans.jsx
+++ src/Pages/Vans.jsx
@@ -19,7 +19,13 @@ function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={van.id}>
+            <Link 
+                to={van.id} 
+                state={{ 
+                    search: `?${searchParams.toString()}`, 
+                    type: typeFilter 
+                }}
+            >
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
@@ -41,7 +47,6 @@ function Vans() {
         })
     }
 
-
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
@@ -49,19 +54,22 @@ function Vans() {
                 <button
                     onClick={() => handleFilterChange("type", "simple")}
                     className={
-                        `van-type simple ${typeFilter === "simple" ? "selected" : ""}`
+                        `van-type simple 
+                        ${typeFilter === "simple" ? "selected" : ""}`
                     }
                 >Simple</button>
                 <button
                     onClick={() => handleFilterChange("type", "luxury")}
                     className={
-                        `van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`
+                        `van-type luxury 
+                        ${typeFilter === "luxury" ? "selected" : ""}`
                     }
                 >Luxury</button>
                 <button
                     onClick={() => handleFilterChange("type", "rugged")}
                     className={
-                        `van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`
+                        `van-type rugged 
+                        ${typeFilter === "rugged" ? "selected" : ""}`
                     }
                 >Rugged</button>
 
