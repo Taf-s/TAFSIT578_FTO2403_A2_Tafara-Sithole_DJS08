439381dec44832d148a511184da71fa1c3930bd2
taf-s <tafara.sithole@gmail.com>
2024-08-07 15:06:31 +0200

    created footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <footer>
+            &#169; 2022 #VANLIFE
+        </footer>
+    )
+}
+
+export default Footer
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
 
 
 function Layout() {
     return (
-            <>
-                <Header />
-                <Outlet />
-            </>
+        <div className="site-wrapper">
+        <Header />
+        <main>
+            <Outlet />
+        </main>
+        <Footer />
+    </div>
     )
 }
 
